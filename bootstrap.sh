9e366ff2fb237b3d3fb6d6d6a6e2c2ab53b0570a
Paul James Cleary <pauljamescleary@gmail.com>
2019-01-08 16:21:50 -0500

    Adding bumpversion

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
-
-if [ ! -d "./.virtualenv" ]; then
-    echo "Creating virtualenv..."
-    virtualenv --clear --python="$(which python3)" ./.virtualenv
-fi
+echo "Creating virtualenv..."
+virtualenv --clear --python="$(which python3)" ./.virtualenv
 
 echo "Installing dependencies..."
 python3 setup.py install
+pip install bumpversion
